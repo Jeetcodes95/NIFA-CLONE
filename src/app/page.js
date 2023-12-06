37c44d5f48917904c6a93d46c4b9b0e31dc08372
Jeetendra Kumar Barman <jeet@Jeetendras-MacBook-Air.local>
2023-12-06 16:54:14 +0530

    imports changed

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 "use client"
 
-import HeaderTop from "@/components/HeaderTop/HeaderTop.js";
-import Navbar from "@/components/Navbar/Navbar.js"
-import Content from '@/components/Content/page.js'
-import HomeSwiper from "@/components/HomeSwiper/page.js";
-import Footer  from "@/components/Footer/page.js"
+import HeaderTop from "../components/HeaderTop/HeaderTop.js";
+import Navbar from "../components/Navbar/Navbar.js"
+import Content from '../components/Content/page.js'
+import HomeSwiper from "../components/HomeSwiper/page.js";
+import Footer  from "../components/Footer/page.js"
 
 export default function Home() {
   
